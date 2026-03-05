7c1bf281e3503d2df993b5f084dd72c3e44cbfff
Josh-T23 <thaisan2@uwm.edu>
2026-03-05 11:33:47 -0600

    Updated Created Habit Box
    Added the Streak indicator to the Created Habit Box

--- habit-tracker-app/src/habitComponents/habit.jsx
+++ habit-tracker-app/src/habitComponents/habit.jsx
@@ -1,18 +1,21 @@
 import HabitEdit from "./habitEdit";
+import { useEffect } from "react";
 import '../css/habit.css'
 
 function Habit(props){
+    const dynamicFontSize = props.name.length > 15 ? "20px" : "30px";
   return(
     <div>
       <div className ="habit-created">
         <h1 style={{fontSize: "64px"}}>📝&nbsp;</h1>
         <div className='habit-info'>
             <h1 className='habit-name'
-            style={{fontSize: "30px", color: "black"}}>{props.name}</h1>
+            style={{fontSize: dynamicFontSize, color: "black"}}>{props.name}</h1>
             <h2 
             style={{fontSize: "24px", color: "red"}}>
               Goal: {props.goal ? props.goal : "0"}/{props.period ? props.period : "week"}</h2>
         </div>
+        <h3 className="habit-streak" title="Habit Streak">🔥{props.streak ? props.streak : "0"}</h3>
         <HabitEdit />
       </div>
       <br />
